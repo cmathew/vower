d1539d9db96a406cd534de26fac67c097acd82e1
cmathew <chris.w.mathew@gmail.com>
2018-12-02 09:21:13 -0800

    add staging environment

---
+++ config/environments/staging.rb
@@ -0,0 +1,36 @@
+Rails.application.configure do  
+    # Do not eager load code on boot.
+    config.eager_load = false
+  
+    # Show full error reports.
+    config.consider_all_requests_local = true
+  
+    # Store uploaded files on the local file system (see config/storage.yml for options)
+    config.active_storage.service = :local
+  
+    # Raise error if the mailer can't send.
+    config.action_mailer.raise_delivery_errors = true
+  
+    config.action_mailer.perform_caching = false
+  
+    # Print deprecation notices to the Rails logger.
+    config.active_support.deprecation = :log
+  
+    # Raise an error on page load if there are pending migrations.
+    config.active_record.migration_error = :page_load
+  
+    # Highlight code that triggered database queries in logs.
+    config.active_record.verbose_query_logs = true
+  
+    # Debug mode disables concatenation and preprocessing of assets.
+    # This option may cause significant delays in view rendering with a large
+    # number of complex assets.
+    config.assets.debug = true
+  
+    # Suppress logger output for asset requests.
+    config.assets.quiet = true
+  
+    # Raises error for missing translations
+    config.action_view.raise_on_missing_translations = true
+  end
+  
