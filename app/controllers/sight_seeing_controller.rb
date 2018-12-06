7c422d4d9c09dc60baf68c19797441c2d2eb3680
cmathew <chris.w.mathew@gmail.com>
2018-12-06 00:53:59 -0800

    give each page a basic template and route

---
+++ app/controllers/sight_seeing_controller.rb
@@ -0,0 +1,2 @@
+class SightSeeingController < ApplicationController
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
   get '/', to: 'home#index'
+  get '/home', to: 'home#index'
   get '/wedding', to: 'wedding#index'
+  get '/accommodations', to: 'accommodations#index'
+  get '/sight_seeing', to: 'sight_seeing#index'
+  get '/other_events', to: 'other_events#index'
+  get '/travel', to: 'travel#index'
+  get '/photos', to: 'photos#index'
+  get '/registry', to: 'registry#index'
+  get '/rsvp', to: 'rsvp#index'
   get '/health_check', to: 'health_check#index'
 end
