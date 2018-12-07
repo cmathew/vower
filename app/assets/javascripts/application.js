2f1e5326f490d1951297adc17e2a0201b1ff88b7
cmathew <chris.w.mathew@gmail.com>
2018-12-06 23:13:57 -0800

    add carousel to home page

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,12 +14,3 @@
 //= require rails-ujs
 //= require activestorage
 //= require turbolinks
-//= require html5
-//= require jquery-1.7.1.min
-//= require jquery.easing.1.3
-//= require jquery.galleriffic
-//= require jquery.opacityrollover
-//= require tms-0.3
-//= require tms_presets
-
-
---
+++ app/assets/javascripts/home.js
@@ -0,0 +1,7 @@
+//= require html5
+//= require jquery-1.7.1.min
+//= require jquery.easing.1.3
+//= require jquery.galleriffic
+//= require jquery.opacityrollover
+//= require tms-0.3
+//= require tms_presets
