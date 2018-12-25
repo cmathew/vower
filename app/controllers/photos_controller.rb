8943db35ad9c5b1e99dec6a926e9fc1062f6a377
Samantha Nano <sgnano21@gmail.com>
2018-12-25 00:37:52 -0800

    initial display

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,5 +1,6 @@
 class PhotosController < ApplicationController
 	def index
 		@activeTab="photos"
+		@photosArray=Dir.glob('app/assets/images/other-photos/photo*.jpeg').sort.map{ |path| path.split('/').last }
 	end
 end
